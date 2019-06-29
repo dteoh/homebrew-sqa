bc80d3352a4fdded4449ab4aedb14e4ca30c48f5
Douglas Teoh <douglas@dteoh.com>
2019-06-29 14:49:51 +0900

    Release 0.7.3

--- Casks/slowquitapps.rb
+++ Casks/slowquitapps.rb
@@ -1,6 +1,6 @@
 cask 'slowquitapps' do
-  version '0.7.2'
-  sha256 '89a1331a62a9426f490ac02e49a2dacadb7291ee7a1ed74afa83d51adb242fa9'
+  version '0.7.3'
+  sha256 '607f54c971dda8e48e39884095e15201c71114e37b54bfe1c3e1d1a3b62e617e'
 
   url "https://github.com/dteoh/SlowQuitApps/releases/download/v#{version}/SlowQuitApps.zip"
   name 'SlowQuitApps'
